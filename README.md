c37db1bf412b3972b1378a20480068e604e033f1
rohitnegi1028 <rohitnegi1028@gmail.com>
2021-07-28 23:11:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-oqed4p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-oqed4p)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<div>Test</div>, document.getElementById('root'));
